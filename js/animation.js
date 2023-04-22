76a7c4bb078ebd101557f3be864c8a74fda1c85b
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-22 20:28:39 +0100

    Update website
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

---
+++ js/animation.js
@@ -0,0 +1,65 @@
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function title_animation() {
+    const title = document.getElementById("title");
+    const titleText = title.innerHTML;
+    title.innerHTML = "";
+    
+    for (let i = 0; i < titleText.length; i++) {
+      const charSpan = document.createElement("span");
+      charSpan.innerText = titleText.charAt(i);
+      charSpan.style.opacity = 0;
+      title.appendChild(charSpan);
+    }
+  
+    let charIndex = 0;
+    let timer = setInterval(() => {
+      const charSpans = title.querySelectorAll("span");
+      const currentChar = charSpans[charIndex];
+      currentChar.style.opacity = 1;
+      charIndex++;
+  
+      if (charIndex === charSpans.length) {
+        clearInterval(timer);
+      }
+    }, 50);
+  }
+  
+title_animation();
+function fadeInElements() {
+    const elements = document.querySelectorAll("#fade_in");
+    let index = 0;
+
+    const fadeInNext = function () {
+        if (index < elements.length) {
+            elements[index].style.opacity = 0;
+            elements[index].style.display = "block";
+            elements[index].style.transition = "opacity 1s ease-in-out";
+            elements[index].style.opacity = 1;
+            index++;
+            setTimeout(fadeInNext, 250);
+        }
+    };
+
+    fadeInNext();
+}
+fadeInElements();
+
+function growDiv() {
+    var divs = document.getElementsByClassName("repo_card");
+    for (var i = 0; i < divs.length; i++) {
+        divs[i].addEventListener("mouseover", function () {
+            this.style.zIndex = "1";
+            this.style.transform = "scale(1.1)";
+            this.style.boxShadow = "0 0 10px 1px rgba(255, 255, 255, 0.5)";
+        });
+        divs[i].addEventListener("mouseout", function () {
+            this.style.zIndex = "0";
+            this.style.transform = "scale(1)";
+            this.style.boxShadow = "0 0 10px 1px rgba(0, 0, 0, 0.5)";
+        });
+    }
+}
+growDiv();
