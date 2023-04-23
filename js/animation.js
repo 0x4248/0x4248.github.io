b2377a283acdb80e9ec82f13c8fa9134d85d9b00
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-23 10:33:02 +0100

    Update website
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- js/animation.js
+++ js/animation.js
@@ -32,14 +32,14 @@ function fadeInElements() {
     const elements = document.querySelectorAll("#fade_in");
     let index = 0;
 
-    const fadeInNext = function () {
+    const fadeInNext = async function () {
         if (index < elements.length) {
             elements[index].style.opacity = 0;
             elements[index].style.display = "block";
             elements[index].style.transition = "opacity 1s ease-in-out";
             elements[index].style.opacity = 1;
             index++;
-            setTimeout(fadeInNext, 250);
+            setTimeout(fadeInNext, 500);
         }
     };
 
@@ -54,12 +54,23 @@ function growDiv() {
             this.style.zIndex = "1";
             this.style.transform = "scale(1.1)";
             this.style.boxShadow = "0 0 10px 1px rgba(255, 255, 255, 0.5)";
+            
+            var otherDivs = document.querySelectorAll(".repo_card:not(:hover)");
+            for (var j = 0; j < otherDivs.length; j++) {
+                otherDivs[j].style.filter = "blur(5px)"; // Adjust the blur radius as needed
+            }
         });
         divs[i].addEventListener("mouseout", function () {
             this.style.zIndex = "0";
             this.style.transform = "scale(1)";
             this.style.boxShadow = "0 0 10px 1px rgba(0, 0, 0, 0.5)";
+            
+            var otherDivs = document.querySelectorAll(".repo_card:not(:hover)");
+            for (var j = 0; j < otherDivs.length; j++) {
+                otherDivs[j].style.filter = "none";
+            }
         });
     }
 }
+
 growDiv();
