a89718b90d2c635816a858042a15e11bd28fba3c
awesomelewis2007 <awesomelewis2007@users.noreply.github.com>
2023-04-29 12:36:54 +0100

    Update animation
    Signed-off-by: awesomelewis2007 <awesomelewis2007@users.noreply.github.com>

--- js/animation.js
+++ js/animation.js
@@ -3,30 +3,29 @@ function sleep(ms) {
 }
 
 async function title_animation() {
-    const title = document.getElementById("title");
-    const titleText = title.innerHTML;
+    const titleText = "Hi, I'm Lewis";
     title.innerHTML = "";
-    
+
     for (let i = 0; i < titleText.length; i++) {
-      const charSpan = document.createElement("span");
-      charSpan.innerText = titleText.charAt(i);
-      charSpan.style.opacity = 0;
-      title.appendChild(charSpan);
+        const charSpan = document.createElement("span");
+        charSpan.innerText = titleText.charAt(i);
+        charSpan.style.opacity = 0;
+        title.appendChild(charSpan);
     }
-  
+
     let charIndex = 0;
     let timer = setInterval(() => {
-      const charSpans = title.querySelectorAll("span");
-      const currentChar = charSpans[charIndex];
-      currentChar.style.opacity = 1;
-      charIndex++;
-  
-      if (charIndex === charSpans.length) {
-        clearInterval(timer);
-      }
+        const charSpans = title.querySelectorAll("span");
+        const currentChar = charSpans[charIndex];
+        currentChar.style.opacity = 1;
+        charIndex++;
+
+        if (charIndex === charSpans.length) {
+            clearInterval(timer);
+        }
     }, 50);
-  }
-  
+}
+
 title_animation();
 function fadeInElements() {
     const elements = document.querySelectorAll("#fade_in");
@@ -54,7 +53,7 @@ function growDiv() {
             this.style.zIndex = "1";
             this.style.transform = "scale(1.1)";
             this.style.boxShadow = "0 0 10px 1px rgba(255, 255, 255, 0.5)";
-            
+
             var otherDivs = document.querySelectorAll(".repo_card:not(:hover)");
             for (var j = 0; j < otherDivs.length; j++) {
                 otherDivs[j].style.filter = "brightness(0.4)";
@@ -64,7 +63,7 @@ function growDiv() {
             this.style.zIndex = "0";
             this.style.transform = "scale(1)";
             this.style.boxShadow = "0 0 10px 1px rgba(0, 0, 0, 0.5)";
-            
+
             var otherDivs = document.querySelectorAll(".repo_card:not(:hover)");
             for (var j = 0; j < otherDivs.length; j++) {
                 otherDivs[j].style.filter = "none";
