511c5b1a5cf2806637b456cbc44df741abc30162
0x4248 <60709927+0x4248@users.noreply.github.com>
2024-06-26 08:18:21 +0100

    Update typewriter.js
    Signed-off-by: 0x4248 <60709927+0x4248@users.noreply.github.com>

--- js/typewriter.js
+++ js/typewriter.js
@@ -10,6 +10,8 @@ let i = 0;
 let speed = 100;
 let cursor = document.querySelector('.blinking-cursor');
 let helloIm = document.getElementById('hello-im');
+let other = ["Programmer", "Cybersecurity enthusiast", "Linux lover", "Uses python too much", "Loves anything open source", "Furry", "Hacker", "Developer", "Tech enthusiast", "Demiboy", "Loves dogs"];
+let otherIndex = 0;
 
 function typeWriter() {
     if (i < text.length) {
@@ -17,7 +19,46 @@ function typeWriter() {
         i++;
         setTimeout(typeWriter, speed);
     } else {
-        cursor.style.display = 'none';
+        setTimeout(eraseText, 2000);
     }
 }
+
+function eraseText() {
+    if (i >= 0) {
+        helloIm.innerHTML = text.substring(0, i) + '<span class="blinking-cursor">_</span>';
+        i--;
+        setTimeout(eraseText, speed / 2);
+    } else {
+        setTimeout(typeOther, 500);
+    }
+}
+
+function typeOther() {
+    let currentWord = other[otherIndex];
+    let j = 0;
+
+    function typeWord() {
+        if (j <= currentWord.length) {
+            helloIm.innerHTML = currentWord.substring(0, j) + '<span class="blinking-cursor">_</span>';
+            j++;
+            setTimeout(typeWord, speed);
+        } else {
+            setTimeout(eraseWord, 2000);
+        }
+    }
+
+    function eraseWord() {
+        if (j >= 0) {
+            helloIm.innerHTML = currentWord.substring(0, j) + '<span class="blinking-cursor">_</span>';
+            j--;
+            setTimeout(eraseWord, speed / 2);
+        } else {
+            otherIndex = (otherIndex + 1) % other.length;
+            setTimeout(typeOther, 500);
+        }
+    }
+
+    typeWord();
+}
+
 setTimeout(typeWriter, 2000);
